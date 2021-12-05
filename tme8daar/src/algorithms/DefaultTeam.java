24d26dfaaa0746843b4d16fe5ccf30d11407d7a4
Lounes.Brahimi <lounes.brahimi@etu.sorbonne-universite.fr>
2021-12-05 19:56:08 +0100

    improved solution (more speed)

--- tme8daar/src/algorithms/DefaultTeam.java
+++ tme8daar/src/algorithms/DefaultTeam.java
@@ -23,13 +23,15 @@ public class DefaultTeam {
 	  }
 	  return pointMost;
   }
-	
+  
   public ArrayList<Point> glouton(ArrayList<Point> points, int edgeThreshold){
-		Evaluation ev = new Evaluation();
-	    ArrayList<Point> fvs = new ArrayList<Point>();
-	    ArrayList<Point> tmpPoints = (ArrayList<Point>) points.clone();
-	    ArrayList<Point> bestFvs = new ArrayList<Point>();
-	    for (int i = 0; i < 10; i++) {
+		this.pointsNotInFvs = null;
+	    Evaluation ev = new Evaluation();
+	    ArrayList<Point> fvs = new ArrayList<Point>(); 
+	    ArrayList<Point> bestFvs = new ArrayList<Point>(); 
+	    for (int i = 0; i < 100; i++) {
+	    	Collections.shuffle(points, new Random(System.nanoTime()));
+	    	ArrayList<Point> tmpPoints = (ArrayList<Point>) points.clone();
 		    while (!(ev.isValid(points, bestFvs, edgeThreshold))) {
 		    	Point pointMost = mostConnectedPoint(tmpPoints, edgeThreshold);
 		    	bestFvs.add(pointMost);
@@ -44,49 +46,7 @@ public class DefaultTeam {
 		    	pointsNotInFvs = null;
 		    	pointsNotInFvs = tmpPoints;
 		    }
-		    Collections.shuffle(points, new Random(System.nanoTime()));
 		}
-	  
-	 // remove 1 add 0 ****************************
-	    boolean fvsAmeliorer = false;
-	    boolean finishedAllPossibilities = false;
-	    while (!finishedAllPossibilities) {
-    		ArrayList<Point> newFvs = fvs;
-    		ArrayList<Point> newPointsNotInFvs = this.pointsNotInFvs;
-	        for (int j = 0; j < fvs.size(); j++) {
-	          //  for (int k = 1; k < fvs.size(); k++) {
-	        		if (this.pointsNotInFvs != null) {
-	        			for (int i = 0; i < this.pointsNotInFvs.size(); i++) {
-	        				ArrayList<Point> tmpFvs = (ArrayList<Point>) fvs.clone(); 
-	        				removeElement(tmpFvs, fvs.get(j));
-	        				tmpFvs.add(this.pointsNotInFvs.get(i));
-	        				if (ev.isValid(points, tmpFvs, edgeThreshold)) {
-	        					fvsAmeliorer = true;
-	        					newFvs = tmpFvs;
-	        					removeElement(newPointsNotInFvs, this.pointsNotInFvs.get(i));
-	        					newPointsNotInFvs.add(fvs.get(j));
-	        				//	newPointsNotInFvs.add(fvs.get(k));
-	        					// to break
-	        					i = this.pointsNotInFvs.size();
-	        					j = fvs.size();
-	        				//	k = fvs.size();
-	        				}
-	        				tmpFvs = null;
-	    				}
-	        		}
-	        //	}
-	    	}
-	        if (fvs.size() == newFvs.size()) break;
-	        fvs = newFvs;
-	        this.pointsNotInFvs = newPointsNotInFvs;
-	        if (!fvsAmeliorer) {
-	        	finishedAllPossibilities = true;
-	        }
-	       // System.out.println("glouton fvs size : "+ fvs.size());
-	    }
-	   // System.out.println("2 fin glouton"+ fvs.size()); 
-	    //-------------------------------------		
-	    
 	    return fvs;
   }
   
@@ -129,138 +89,64 @@ public class DefaultTeam {
   }
   
   //--------------------------------------------------
+
+  public ArrayList<Point> remove2add1(ArrayList<Point> points, int edgeThreshold){
+	Evaluation ev = new Evaluation();
+	ArrayList<Point> bestFvs = glouton(points, edgeThreshold);
+	for (int y = 0; y < 5; y++) {
+		ArrayList<Point> naifFvs = glouton(points, edgeThreshold);
+		ArrayList<Point> memPointsNotInFvs = (ArrayList<Point>) this.pointsNotInFvs.clone();
+	    boolean fvsAmeliorer = false;
+	    boolean finishedAllPossibilities = false;
+    	Collections.shuffle(naifFvs, new Random(System.nanoTime()));
+    	Collections.shuffle(this.pointsNotInFvs, new Random(System.nanoTime()));
+	    while(!finishedAllPossibilities) {
+    		ArrayList<Point> newFvs = (ArrayList<Point>) naifFvs.clone();
+    		ArrayList<Point> newPointsNotInFvs = (ArrayList<Point>) memPointsNotInFvs.clone();
+    		int j=0, k=1;
+    		while(j < (naifFvs.size()-1) || k < naifFvs.size()) {
+    				if (naifFvs.get(j) != naifFvs.get(k) && this.pointsNotInFvs != null) {
+    					int i=0;
+    					while(i < this.pointsNotInFvs.size()) {
+    						ArrayList<Point> tmpFvs = (ArrayList<Point>) naifFvs.clone();
+    						removeTwoElement(tmpFvs, naifFvs.get(j), naifFvs.get(k));
+    						tmpFvs.add(this.pointsNotInFvs.get(i));
+    						if (ev.isValid(points, tmpFvs, edgeThreshold)) {
+	        					fvsAmeliorer = true;
+	        					newFvs = (ArrayList<Point>) tmpFvs.clone();
+	        					removeElement(newPointsNotInFvs, this.pointsNotInFvs.get(i));
+	        					newPointsNotInFvs.add(naifFvs.get(j));
+	        					newPointsNotInFvs.add(naifFvs.get(k));
+	        					// to break
+	        					i = this.pointsNotInFvs.size();
+	        					j = naifFvs.size();
+	        					k = naifFvs.size();
+    						}
+    						tmpFvs = null;
+    						i++;
+    					}
+    				}
+    			j++;k++;
+    		}
+	        if (naifFvs.size() == newFvs.size()) break;
+	        naifFvs = (ArrayList<Point>) newFvs.clone();
+	        this.pointsNotInFvs = (ArrayList<Point>) newPointsNotInFvs.clone();
+	        if (!fvsAmeliorer) {
+	        	finishedAllPossibilities = true;
+	        }
+	    }
+	    	if (naifFvs.size() < bestFvs.size())
+	    		bestFvs = (ArrayList<Point>) naifFvs.clone();
+	    	System.out.println("size : "+ bestFvs.size());
+	  }
+	  return bestFvs;
+  }
   
   public ArrayList<Point> calculFVS(ArrayList<Point> points, int edgeThreshold) {
-	try {int cpt = 0;
 		Evaluation ev = new Evaluation();
-	    ArrayList<Point> fvs = new ArrayList<Point>();
-	    
-	    // first non optimal solution
-	   // System.out.println("################");
-	    fvs = glouton(points, edgeThreshold);
-	   // System.out.println("################2");
-	    ArrayList<Point> fvs1 = (ArrayList<Point>) fvs.clone();
-	    ArrayList<Point> fvsmem = (ArrayList<Point>) fvs.clone();
-	    ArrayList<Point> memPointsNotInFvs = (ArrayList<Point>) this.pointsNotInFvs.clone();
-	    
-	    ArrayList<Point> bestFvs = new ArrayList<Point>();
-	    //-----------------------------------------------------------
-	    //-------------------------------------------------------
-	    
-	    
-	    int loopT = 5;
-	    
-	    for (int y = 0; y < loopT; y++) {
-	    	fvs1 = (ArrayList<Point>) fvsmem.clone();
-	    	ArrayList<Point> pointsNotInFvs1 = (ArrayList<Point>) this.pointsNotInFvs.clone();
-	    	Collections.shuffle(fvs1, new Random(System.nanoTime()));
-	    	Collections.shuffle(this.pointsNotInFvs, new Random(System.nanoTime()));
-	    	ArrayList<Point> fvs12 = (ArrayList<Point>) fvs1.clone();
-	    	ArrayList<Point> pointsNotInFvs2 = (ArrayList<Point>) this.pointsNotInFvs.clone();
-
-		    //-----------------------------------------------------
-		     boolean fvsAmeliorer = false;
-		     boolean finishedAllPossibilities = false;
-		    while (!finishedAllPossibilities) {
-	    		ArrayList<Point> newFvs = (ArrayList<Point>) fvs1.clone();
-	    		ArrayList<Point> newPointsNotInFvs = (ArrayList<Point>) memPointsNotInFvs.clone();
-		        for (int j = 0; j < (fvs1.size()-1); j++) {
-		            for (int k = 1; k < fvs1.size(); k++) {
-		        		if (fvs1.get(j) != fvs1.get(k) && this.pointsNotInFvs != null) {
-		        			for (int i = 0; i < this.pointsNotInFvs.size(); i++) {
-		        				ArrayList<Point> tmpFvs = (ArrayList<Point>) fvs1.clone(); 
-		        				removeTwoElement(tmpFvs, fvs1.get(j), fvs1.get(k));
-		        				tmpFvs.add(this.pointsNotInFvs.get(i));
-		        				if (ev.isValid(points, tmpFvs, edgeThreshold)) {
-		        					fvsAmeliorer = true;
-		        					newFvs = tmpFvs;
-		        					removeElement(newPointsNotInFvs, this.pointsNotInFvs.get(i));
-		        					newPointsNotInFvs.add(fvs1.get(j));
-		        					newPointsNotInFvs.add(fvs1.get(k));
-		        					// to break
-		        					i = this.pointsNotInFvs.size();
-		        					j = fvs.size();
-		        					k = fvs.size();
-		        				}
-		        				tmpFvs = null;
-		    				}
-		        		}
-		        	}
-		    	}
-		        if (fvs1.size() == newFvs.size()) break;
-		        fvs1 = newFvs;
-		        pointsNotInFvs1 = newPointsNotInFvs;
-		        if (!fvsAmeliorer) {
-		        	finishedAllPossibilities = true;
-		        }
-		    }
-		        System.out.println("fvs1 size : "+ fvs1.size());
-		        		        
-		        fvsAmeliorer = false;
-			    finishedAllPossibilities = false;
-			    while (!finishedAllPossibilities) {
-		    		ArrayList<Point> newFvs2 = (ArrayList<Point>) fvs12.clone();
-		    		ArrayList<Point> newPointsNotInFvs2 = (ArrayList<Point>) memPointsNotInFvs.clone();
-			        for (int j = (fvs12.size()-2); j >= 0; j--) {
-			            for (int k = (fvs12.size()-1); k >= 1; k--) {
-			        		if (fvs12.get(j) != fvs12.get(k) && this.pointsNotInFvs != null) {
-			        			for (int i = 0; i < this.pointsNotInFvs.size(); i++) {
-			        				ArrayList<Point> tmpFvs = (ArrayList<Point>) fvs12.clone(); 
-			        				removeTwoElement(tmpFvs, fvs12.get(j), fvs12.get(k));
-			        				tmpFvs.add(this.pointsNotInFvs.get(i));
-			        				if (ev.isValid(points, tmpFvs, edgeThreshold)) {
-			        					fvsAmeliorer = true;
-			        					newFvs2 = tmpFvs;
-			        					removeElement(newPointsNotInFvs2, this.pointsNotInFvs.get(i));
-			        					newPointsNotInFvs2.add(fvs12.get(j));
-			        					newPointsNotInFvs2.add(fvs12.get(k));
-			        					// to break
-			        					i = this.pointsNotInFvs.size();
-			        					j = -1;
-			        					k = -1;
-			        				}
-			        				tmpFvs = null;
-			    				}
-			        		}
-			        	}
-			    	}
-			        if (fvs12.size() == newFvs2.size()) break;
-			        fvs12 = newFvs2;
-			        pointsNotInFvs2 = newPointsNotInFvs2;
-			        if (!fvsAmeliorer) {
-			        	finishedAllPossibilities = true;
-			        }
-			    
-			        
-		    }//___________________________________
-			    System.out.println("fvs12 size : "+ fvs12.size());
-		    //-------------------------------------		
-		    if (y == 0) {
-		    	if (fvs1.size() < fvs12.size())
-		    		bestFvs = (ArrayList<Point>) fvs1.clone();
-		    	else 
-		    		bestFvs = (ArrayList<Point>) fvs12.clone();
-		    	
-		    } else if (fvs1.size() < bestFvs.size()){
-		    	if (fvs1.size() < fvs12.size())
-		    		bestFvs = (ArrayList<Point>) fvs1.clone();
-		    	else 
-		    		bestFvs = (ArrayList<Point>) fvs12.clone();
-		    }
-		    System.out.println("best : "+ bestFvs.size());
-		    if (bestFvs.size() <= 79) y = loopT;
-		    if ((y == (loopT-1)) && bestFvs.size()>= 85 ) loopT++;
-		   // System.out.println("size 2 : "+ fvs12.size());
-		   // Collections.shuffle(points, new Random(System.nanoTime()));
-		}
-
-
-	    
-	    return bestFvs;	
-	} catch (Exception e) {
-		e.printStackTrace();
-	}
-	return null;
+	    ArrayList<Point> fvs = new ArrayList<Point>();	    
+	    fvs = remove2add1(points, edgeThreshold);
+	    return fvs;	
   }
   
   public ArrayList<Point> removeElement(ArrayList<Point> points, Point p)
